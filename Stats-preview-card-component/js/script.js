cf19cbb87003383a1e3e43bf9984314dcf85c06a
Leandro <leagrisalena@gmail.com>
2021-05-13 00:58:30 -0300

    Stats-preview-card-component - js

--- Stats-preview-card-component/js/script.js
+++ Stats-preview-card-component/js/script.js
@@ -1,14 +1,3 @@
-/* WebFont.load({
-    google: {
-        families: [
-            'Inter', 'Lexend Deca'
-        ]
-    },
-    active: function () {
-        document.querySelector('.preview__card-container').classList.add('preview__card-container--show')
-    }
-}) */
-
 /* class FontsLoaded {
     constructor(clase) {
         this.clase = clase;
@@ -32,10 +21,11 @@ const fontsOnLoad = new FontsLoaded('preview__card-container--show'); */
 
 var FontsLoaded = function (clase) {
     this.clase = clase;
+    this.previewCardContainer = document.querySelector('.preview__card-container');
 }
 
 FontsLoaded.prototype.init = function () {
-    var clase = this.clase;
+    var self = this;
     WebFont.load({
         google: {
             families: [
@@ -43,10 +33,24 @@ FontsLoaded.prototype.init = function () {
             ]
         },
         active: function () {
-            document.querySelector('.preview__card-container').classList.add(clase)
+            self.previewCardContainer.classList.add(self.clase)
         }
     });
 }
 
+FontsLoaded.prototype.getData = function () {
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "https://www.lgm-studioweb.com.ar/Frontend-Mentor/data.json", true);
+    ajax.send();
+    ajax.addEventListener("load", (function () {
+        var data = JSON.parse(ajax.responseText);
+        this.previewCardContainer.innerHTML = '<div class="preview__card-copy"><div class="preview__card-copy-container"><h1 class="preview__card-headline">' + data.h1 + '</h1><p class="preview__card-paragraph">' + data.p + '</p><ul class="preview__card-stats"><li class="preview__card-list">' + data.lists[0] + '</li><li class="preview__card-list">' + data.lists[1] + '</li><li class="preview__card-list">' + data.lists[2] + '</li></ul></div></div><div class="preview__card-image" role="img" aria-label=""></div>'
+    }).bind(this));
+    ajax.addEventListener("error", function () {
+        console.error(new Error("hubo un error"));
+    })
+}
+
 var fontsOnLoad = new FontsLoaded('preview__card-container--show');
 fontsOnLoad.init();
+fontsOnLoad.getData();
