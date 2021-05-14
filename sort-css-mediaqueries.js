0bcd12c16dd360c325d7dbb2c5323194a954d55a
Leandro Grisalena Morello <leagrisa@WKMZT0DD141F.global.publicisgroupe.net>
2021-05-14 19:13:39 -0300

    3-column-preview-card-component-main - html

--- sort-css-mediaqueries.js
+++ sort-css-mediaqueries.js
@@ -1,12 +1,12 @@
 "use strict;"
 const fs = require("fs"),
     mqpacker = require("css-mqpacker"),
-    result = mqpacker.pack(fs.readFileSync("./Stats-preview-card-component/css/style.css", "utf8"), {
-        from: "./Stats-preview-card-component/css/style.css",
+    result = mqpacker.pack(fs.readFileSync("./3-column-preview-card-component-main/css/style.css", "utf8"), {
+        from: "./3-column-preview-card-component-main/css/style.css",
         map: {
             inline: false
         },
         sort: true,
         to: "to.css"
     });
-fs.writeFileSync("./Stats-preview-card-component/css/style.css", result.css);
+fs.writeFileSync("./3-column-preview-card-component-main/css/style.css", result.css);
